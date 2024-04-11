a78c463e7d7400feb02150c51f605b38784bfed1
Ashutosh-777 <ashutosh.ghosty@gmail.com>
2024-04-12 00:29:50 +0530

    SOCIAL TEXTFIELD

--- lib/widgets/social_textfield.dart
+++ lib/widgets/social_textfield.dart
@@ -10,36 +10,33 @@ class SocialTextField extends StatefulWidget {
 class _SocialTextFieldState extends State<SocialTextField> with SingleTickerProviderStateMixin {
   late FocusNode _focusNode;
   bool _showSuffixIcon = false;
-
+  bool green=false;
   @override
   void initState() {
     super.initState();
     _focusNode = FocusNode();
-
     _focusNode.addListener(() {
       setState(() {
         _showSuffixIcon = _focusNode.hasFocus;
       });
     });
-
   }
-
-
   @override
   void dispose() {
     _focusNode.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return  Container(
       decoration: BoxDecoration(
-        color: const Color.fromRGBO(224, 224, 224, 1),
+        color: green ? Colors.green :const Color.fromRGBO(224, 224, 224, 1),
         borderRadius: BorderRadius.circular(12),
       ),
       alignment: Alignment.center,
-      child: TextField(
+      child: green ? Container(
+        height: 40,
+          child: Center(child: const Text('Socials updated Successfully'))) :TextField(
         focusNode: _focusNode,
         maxLines: null,
         style: TextStyle(
@@ -49,7 +46,14 @@ class _SocialTextFieldState extends State<SocialTextField> with SingleTickerProv
             hintText: 'Paste Your ${widget.name} Profile Here',
             suffixIcon: _showSuffixIcon ?
             GestureDetector(
-                child: const Icon(Icons.check)) : null,
+              onTap: (){
+                setState(() {
+                  green = true;
+                  print("green");
+                });
+              },
+                child: const Icon(Icons.check)
+            ) : null,
             hintStyle: const TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.normal,
